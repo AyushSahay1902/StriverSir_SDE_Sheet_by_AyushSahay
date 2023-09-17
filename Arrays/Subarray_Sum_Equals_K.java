2f1cb4ff75eff173e2e55d0fef911a9f04a1a700
AyushSahay1902 <sahayayush385@gmail.com>
2023-09-17 18:50:27 +0530

    commit

---
+++ Arrays/Subarray_Sum_Equals_K.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+public class Subarray_Sum_Equals_K {
+    //Brute force solution - O(n^3), space comlexity - O()
+    // int n = nums.length;
+    // int count = 0;
+    
+    // for(int i=0; i<n; i++){
+    //     for(int j=i; j<n; j++){
+    //         int sum = 0;
+    //         for(int l = i; l<=j; l++){
+    //             sum = sum + nums[l];
+    //         }
+    //         if(sum == k){
+    //             count++;
+    //         }
+    //     }
+    // }
+    
+    // return count;
+
+    //Better solution for the same problem
+    // int n = nums.length;
+    //     int count = 0;
+    //     for(int i=0; i<n; i++){
+    //         int sum = 0;
+    //         for(int j=i; j<n; j++){
+    //             sum = sum + nums[j];
+    //         }    
+    //         if(sum == k){
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+
+    // Optimal Solution would be
+    
+}
