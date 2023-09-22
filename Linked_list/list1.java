60a7fb45563e10d4e56c0a733696406e39424fae
AyushSahay1902 <sahayayush385@gmail.com>
2023-09-22 18:32:34 +0530

    commit

---
+++ Linked_list/list1.java
@@ -0,0 +1,17 @@
+
+package Linked_list;
+import java.util.*;
+public class list1 {
+
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        list.add(6);
+        
+
+    }
+    
+}
+//Linked list is a linear daya structure.
+//Dynamic data structure(grow/shrink).
+
